f40d835ddbe477c216f577d0c31393ed6f58baf9
Sean Simmons <sean@unitcircle.ca>
2021-09-28 12:55:08 -0400

    Initial Commit

---
+++ Konnex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Konnex
+//
+//  Created by Sean Simmons on 2021-09-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
